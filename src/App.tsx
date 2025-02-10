622634b36886689195242a686c97ecb5c5c4af4e
Ali Helmi <ahelmi365@gmail.com>
2025-02-10 23:46:41 +0200

    init layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Layout } from "antd";
+import "./App.css"; // Import the CSS file
+
+const { Header, Footer, Content } = Layout;
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <Layout className="layout">
+      <Header className="header">Header</Header>
+      <Content className="content">Content</Content>
+      <Footer className="footer">Footer</Footer>
+    </Layout>
+  );
 }
 
 export default App;
