76bd72f25f803e1041a60f9311a91d15b0bd0b6f
Ali Helmi <ahelmi365@gmail.com>
2025-02-12 13:49:39 +0200

    add title

--- src/pages/Authors/Authors.tsx
+++ src/pages/Authors/Authors.tsx
@@ -1,5 +1,29 @@
+import { Flex, Row, Col, Button } from "antd";
+import Search from "antd/es/transfer/search";
+import Title from "antd/es/typography/Title";
+
 const Authors = () => {
-  return <div>Author</div>;
+  return (
+    <Flex vertical gap={"1rem"}>
+      <Row justify={"space-between"}>
+        <Col span={20}>
+          <Flex gap={16} justify="start">
+            <Col>
+              <Title level={3} style={{ margin: 0 }}>
+                Authors List
+              </Title>
+            </Col>
+            <Col>
+              <Search />
+            </Col>
+          </Flex>
+        </Col>
+        <Col>
+          <Button>Add New Author</Button>
+        </Col>
+      </Row>
+    </Flex>
+  );
 };
 
 export default Authors;
