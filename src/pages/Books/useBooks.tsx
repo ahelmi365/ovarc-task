53aa988d7307112257868995be7c98d803814a3e
Ali Helmi <ahelmi365@gmail.com>
2025-02-12 14:35:18 +0200

    add hook to show books

---
+++ src/pages/Books/useBooks.tsx
@@ -0,0 +1,161 @@
+import { setIsLoading } from "@store/authSlice/authSlice";
+import { useAppDispatch } from "@store/hooks";
+import {
+  addAuthorNamesToBooks,
+  generateTableFilters,
+  onFilterTable,
+  sortTable,
+} from "@utils/index";
+import {
+  Button,
+  Flex,
+  notification,
+  TableColumnsType,
+  TablePaginationConfig,
+} from "antd";
+import { deleteBook, getBooks } from "apis/books";
+import { useEffect, useState } from "react";
+import { Book } from "types";
+import editIcon from "@assets/svg/editIcon.svg";
+import deleteIcon from "@assets/svg/deleteIcon.svg";
+
+const useBooks = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const dispatch = useAppDispatch();
+
+  const handleDeleteBook = async (id: number) => {
+    dispatch(setIsLoading(true));
+    try {
+      // Assuming deleteBook is an API call to delete the book by id
+      await deleteBook(id);
+      setBooks((prevBooks) => prevBooks.filter((book) => book.id !== id));
+      notification.success({ message: "Book deleted successfully" });
+    } catch (err) {
+      setError(err instanceof Error ? err.message : "An error occurred");
+      notification.error({
+        message: "Failed to delete book, please try again",
+      });
+    } finally {
+      dispatch(setIsLoading(false));
+    }
+  };
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      dispatch(setIsLoading(true));
+      try {
+        const data = await getBooks();
+        const booksWithAuthorNames = await addAuthorNamesToBooks(data);
+        setBooks(booksWithAuthorNames);
+
+        setError(null);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : "An error occurred");
+        notification.error({ message: "Network Error, please try again" });
+      } finally {
+        dispatch(setIsLoading(false));
+      }
+    };
+
+    fetchBooks();
+  }, []);
+
+  const [pagination, setPagination] = useState<TablePaginationConfig>({
+    current: 1,
+    pageSize: 5,
+  });
+
+  const handleTableChange = (newPagination: TablePaginationConfig) => {
+    setPagination(newPagination);
+  };
+
+  const columns: TableColumnsType<Book> = [
+    {
+      title: "Book ID",
+      dataIndex: "id",
+      key: "id",
+      fixed: "left",
+      render: (_, __, index) =>
+        (pagination.current! - 1) * pagination.pageSize! + index + 1,
+    },
+
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      filters: generateTableFilters(books, "name"),
+      onFilter: (value, record) => onFilterTable(value, record, "name"),
+      sorter: (a, b) => sortTable(a, b, "name"),
+      render: (_, record) =>
+        `${record.name.charAt(0).toUpperCase()}${record.name.slice(1)}`,
+    },
+    {
+      title: "Pages",
+      dataIndex: "pages",
+      key: "pages",
+      render: (_, record) => record.page_count,
+      filters: generateTableFilters(books, "page_count"),
+      onFilter: (value, record) => onFilterTable(value, record, "page_count"),
+      sorter: (a, b) => sortTable(a, b, "page_count"),
+    },
+    {
+      title: "Author Name",
+      dataIndex: "authorName",
+      key: "authorName",
+      render: (_, record) => record.authorName,
+      filters: generateTableFilters(books, "authorName"),
+      onFilter: (value, record) => onFilterTable(value, record, "authorName"),
+      sorter: (a, b) => sortTable(a, b, "authorName"),
+    },
+
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Flex gap={16}>
+          <Button
+            size="middle"
+            // variant="outlined"
+            style={{
+              backgroundColor: "#BF5523",
+              borderColor: "#BF5523",
+              color: "#BF5523",
+            }}
+            //   onClick={() => handleViewRequestDetails(record)}
+          >
+            <img
+              src={editIcon}
+              alt="Edit Book"
+              title="Edit Book"
+              width={15}
+              height={15}
+            />
+          </Button>
+          <Button
+            size="middle"
+            style={{
+              backgroundColor: "#BF5523",
+              borderColor: "#BF5523",
+              color: "#BF5523",
+            }}
+            onClick={() => handleDeleteBook(record.id)}
+          >
+            <img
+              src={deleteIcon}
+              alt="Edit Book"
+              title="Delete Book"
+              width={15}
+              height={15}
+            />
+          </Button>
+        </Flex>
+      ),
+      fixed: "right",
+    },
+  ];
+
+  return { books, columns, handleTableChange, pagination };
+};
+
+export default useBooks;
