956348b1805f6f35bf85374cfa4129350344b29b
Ali Helmi <ahelmi365@gmail.com>
2025-02-12 11:52:18 +0200

    add layout to shopcard

--- src/components/ShopCard/ShopCard.tsx
+++ src/components/ShopCard/ShopCard.tsx
@@ -1,5 +1,32 @@
+import { Card, Col, Row } from "antd";
+
 const ShopCard = () => {
-  return <div>ShopCard</div>;
+  return (
+    <Card>
+      <Row>
+        <Col span={8}>Book title side</Col>
+        <Col span={16}>
+          <Row>
+            <Col>Book Title</Col>
+            <Col>by autho name</Col>
+          </Row>
+          <Row>
+            <span>Stores</span>
+            <Col>
+              <span>Store 1</span>
+              <span>Price</span>
+              <button>sell</button>
+            </Col>
+            <Col>
+              <span>Store 1</span>
+              <span>Price</span>
+              <button>sell</button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Card>
+  );
 };
 
 export default ShopCard;
