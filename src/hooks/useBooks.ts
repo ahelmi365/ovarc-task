d7e6008ca9ca46e57681a485cb4c6bdef6c7c888
Ali Helmi <ahelmi365@gmail.com>
2025-02-12 13:19:57 +0200

    add author name to book

--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -1,4 +1,4 @@
-import { getBooks } from "apis/books";
+import { getAuthorName, getBooks } from "apis/books";
 import { useEffect, useState } from "react";
 import { Book } from "types";
 
@@ -7,13 +7,24 @@ const useBooks = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
+  const addAuthorNamesToBooks = async (books: Book[]) => {
+    const booksWithAuthorNames = await Promise.all(
+      books.map(async (book) => {
+        const authorName = await getAuthorName(book.author_id);
+        console.log({ authorName });
+        return { ...book, authorName };
+      })
+    );
+    return booksWithAuthorNames;
+  };
+
   useEffect(() => {
     const fetchBooks = async () => {
       setIsLoading(true);
       try {
         const data = await getBooks();
-        console.log({ data });
-        setBooks(data);
+        const booksWithAuthorNames = await addAuthorNamesToBooks(data);
+        setBooks(booksWithAuthorNames);
         setError(null);
       } catch (err) {
         setError(err instanceof Error ? err.message : "An error occurred");
