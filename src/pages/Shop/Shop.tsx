a4b83c01611c24beb6d14e7293e1053362c1af85
Ali Helmi <ahelmi365@gmail.com>
2025-02-12 11:53:46 +0200

    use books hok to get books and show shopcards

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,7 +1,19 @@
+import ShopCard from "@components/ShopCard/ShopCard";
+import useBooks from "@hooks/useBooks";
+import { Row, Col } from "antd";
 import React from "react";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  const { books, isLoading, error } = useBooks();
+  return (
+    <Row gutter={16}>
+      {books.slice(0, 10)?.map((book) => (
+        <Col sm={24} md={12} lg={8} key={book.id}>
+          <ShopCard />
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default Shop;
